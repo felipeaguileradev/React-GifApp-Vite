78fcfd044f1097eda683fc53c6a1626eefbea558
Felipe Aguilera <felipe.aguilera.dev@gmail.com>
2024-01-05 02:30:09 -0300

    se agregar pruebas unitarias

---
+++ tests/components/GifGrid.test.jsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs.js");
+
+describe("pruebas en <GifGrid />", () => {
+  const category = "One Punch";
+
+  test("debe mostrar el loading inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getByText("Cargando..."));
+    expect(screen.getByText(category));
+    // screen.debug();
+  });
+
+  test("debe mostrar items cuando se cargan las imágnees useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "https://localhost/saitama.jpg",
+      },
+      {
+        id: "123",
+        title: "Goku",
+        url: "https://localhost/goku.jpg",
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole("img").length).toBe(2);
+    // screen.debug();
+  });
+});
